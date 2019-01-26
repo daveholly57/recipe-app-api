434d0be9476f65ddf5fa2452a6e4ed25e6160a66
daveholly57 <dave.holly@verizon.net>
2019-01-26 13:13:45 -0500

    final correction for travis, test, and calc.

--- app/app/calc.py
+++ app/app/calc.py
@@ -4,9 +4,11 @@ def add(x, y):
     
     return x + y 
 
+
 def subtract(x, y):
 
     return y - x  
+
 
 def multiply(x, y):
 
--- app/app/tests.py
+++ app/app/tests.py
@@ -1,14 +1,19 @@
 from django.test import TestCase
 
-from app.calc import add, subtract
+from app.calc import add, subtract, multiply
 
 
 class CalcTests(TestCase):
 
     def test_add_numbers(self):
         # Test that two numbers are added together
-        self.assertEqual(add(3,8), 11)
+        self.assertEqual(add(3, 8), 11)
 
     def test_subtract_numbers(self):
         # Test subtract calc.
-        self.assertEqual(subtract(1,7), 6)
+        self.assertEqual(subtract(1, 7), 6)
+
+    def test_multiply_numbers(self):
+        # yep
+        self.assertEqual(multiply(2, 4), 8)
+    
