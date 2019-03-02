dfa671530df1afdaf9fe36c08a94990509c6fc38
daveholly57 <dave.holly@verizon.net>
2019-03-02 15:52:21 -0500

    Added recipe image upload feature - again.

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -76,7 +76,7 @@ class RecipeViewSet(viewsets.ModelViewSet):
                 serializer.data,
                 status=status.HTTP_200_OK
             )
-    
+
         return Response(
             serializer.errors,
             status=status.HTTP_400_BAD_REQUEST
